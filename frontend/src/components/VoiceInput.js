ec5f8c9bb9b054d09de8937b5c4c41889514e7ad
Jubeerathan <jubeerathan@gmail.com>
2023-09-30 20:35:44 +0530

    improve voice functionality

--- frontend/src/components/VoiceInput.js
+++ frontend/src/components/VoiceInput.js
@@ -3,8 +3,10 @@ import "regenerator-runtime/runtime";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
+import mic_on from "../assets/mic_on_icon.svg";
+import mic_off from "../assets/mic_off_icon.svg";
 
-const VoiceInput = ({ setUserInput }) => {
+const VoiceInput = ({ userInput, setUserInput }) => {
   // Pass the recognition object to the useSpeechRecognition hook
   const {
     transcript,
@@ -16,7 +18,8 @@ const VoiceInput = ({ setUserInput }) => {
   useEffect(() => {
     // Update userInput when a new transcript is received
     if (transcript !== "") {
-      setUserInput(transcript);
+      setUserInput((prevUserInput) => prevUserInput + ` ${transcript}`);
+      resetTranscript();
     }
   }, [transcript, setUserInput]);
 
@@ -42,9 +45,21 @@ const VoiceInput = ({ setUserInput }) => {
     <div>
       {/* <p>Microphone: {listening ? "on" : "off"}</p> */}
       <button onClick={toggleMicrophone}>
-        {isMicrophoneActive ? "Stop" : "Start"}
+        {isMicrophoneActive ? (
+          <img
+            src={mic_off}
+            alt="mic_off"
+            style={{ width: "30px", height: "100%" }}
+          />
+        ) : (
+          <img
+            src={mic_on}
+            alt="mic_on"
+            style={{ width: "30px", height: "100%" }}
+          />
+        )}
       </button>
-      <button onClick={resetTranscript}>Reset</button>
+      {/* <button onClick={resetTranscript}>Reset</button> */}
       {/* <p>{transcript}</p> */}
     </div>
   );
