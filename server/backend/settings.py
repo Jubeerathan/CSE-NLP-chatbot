e1d92ed3e2320063272529229a84e1e83c5ef9ca
Jubeerathan <jubeerathan@gmail.com>
2023-10-05 12:13:20 +0530

    edit settings.py

--- server/backend/settings.py
+++ server/backend/settings.py
@@ -34,6 +34,7 @@ EMAIL_PORT = os.environ.get("EMAIL_PORT")
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
