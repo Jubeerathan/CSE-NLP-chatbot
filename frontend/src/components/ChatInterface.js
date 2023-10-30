7ee02d75b239a2152c39b4212b3e9e6f2200ae89
Jubeerathan <jubeerathan@gmail.com>
2023-10-30 13:33:56 +0530

    add getuserid

--- frontend/src/components/ChatInterface.js
+++ frontend/src/components/ChatInterface.js
@@ -17,18 +17,24 @@ import {
 import ChatMessage from "./ChatMessage";
 // import uom_logo from "../assets/uom_logo.png";
 // import cse_logo from "../assets/cse_logo.png";
-import ChatHeader from "./ChatHeader";
 import VoiceInput from "./VoiceInput";
 import { getUser } from "../services/ChatServices";
 
 const ChatInterface = () => {
   // user ID should be hardcoded here.
 
-  const user_ID= 3;
+  // const user_ID= 3;
   // const userName = "hi";
 
+  let [user_ID, setUser_ID] = useState(999999);
   let [topics, setTopics] = useState([]);
   const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    getUser().then((response) => {
+      setUser_ID(response.data.user_id);
+    });
+  }, []);
 
   useEffect(() => {
     get_conversation_title(user_ID).then((data) => {
@@ -37,22 +43,20 @@ const ChatInterface = () => {
     get_conversation_by_user_id(user_ID).then((data) => {
       setMessages(data);
     });
-  }, []);
+  }, [user_ID]);
 
   const [currentTopic, setCurrentTopic] = useState(
     topics && topics.length > 0 ? topics[0].conversation_title : []
   );
   const [userInput, setUserInput] = useState("");
   const [userTopicInput, setUserTopicInput] = useState("");
-  const [answerFromBot, setAnswerFromBot] = useState("");
+  // const [answerFromBot, setAnswerFromBot] = useState("");
   const [convertMessage, setConvertMessage] = useState({});
   const messagesEndRef = useRef(null);
 
   useEffect(() => {
     scrollToBottom();
-
-  }, [currentTopic,convertMessage]);
-
+  }, [currentTopic, convertMessage]);
 
   const scrollToBottom = () => {
     messagesEndRef.current.scrollIntoView({ behavior: "smooth", block: "end" });
@@ -65,7 +69,7 @@ const ChatInterface = () => {
   // convert messages into useful format inorder to display on chat body.
   if (Object.keys(convertMessage).length === 0 && messages.length > 0) {
     messages.forEach((item) => {
-      const { conversation_title, question, answer, user } = item;
+      const { conversation_title, question, answer } = item;
 
       if (!convertMessage[conversation_title]) {
         convertMessage[conversation_title] = [];
@@ -117,7 +121,7 @@ const ChatInterface = () => {
     event.preventDefault();
 
     const capitalizedUserInput = toSentenceCase(userInput);
-    setUserInput('');
+    setUserInput("");
 
     // Create a new message object
     const userMessage = { message: capitalizedUserInput, sender: "user" };
@@ -158,18 +162,17 @@ const ChatInterface = () => {
 
       // setUserInput("");
 
-      setAnswerFromBot("");
+      // setAnswerFromBot("");
     });
   };
 
   //convert user input into sentence case.
   function toSentenceCase(inputText) {
     // Split the input text into an array of sentences using regular expressions.
-    const sentences = inputText.split(/(\. |\? |\! )/);
+    const sentences = inputText.split(/(\. |\? |! )/);
 
     let result = "";
 
-    // Capitalize the first letter of each sentence and add.
     for (let i = 0; i < sentences.length; i += 2) {
       // Capitalize the first character of the sentence.
       const sentence =
@@ -217,18 +220,27 @@ const ChatInterface = () => {
 
     // Clear the userTopicInput
     setUserTopicInput("");
-    setAnswerFromBot("");
+    // setAnswerFromBot("");
   };
 
   return (
-
     <div style={{ minHeight: "50vh", width: "100%" }}>
-    <Container fluid className="py-1" style={{ height: "100vh", width: "100%" }}>
-      <Row className="d-flex justify-content-center" style={{ height: "100%" }}>
-        <Col md="10" lg="11" xl="12">
-          <Card id="chat1" style={{ borderRadius: "15px", height: "100%", width: "100%" }}>
-          {/* <ChatHeader /> */}
-            {/* <Card.Header
+      <Container
+        fluid
+        className="py-1"
+        style={{ height: "100vh", width: "100%" }}
+      >
+        <Row
+          className="d-flex justify-content-center"
+          style={{ height: "100%" }}
+        >
+          <Col md="10" lg="11" xl="12">
+            <Card
+              id="chat1"
+              style={{ borderRadius: "15px", height: "100%", width: "100%" }}
+            >
+              {/* <ChatHeader /> */}
+              {/* <Card.Header
 
     <Container fluid className="py-1" style={{ height: "100vh" }}>
       <Row className="d-flex justify-content-center" style={{ height: "100%" }}>
@@ -245,151 +257,150 @@ const ChatInterface = () => {
                 alignItems: "center",
               }}
             > */}
-             
-            {/* </Card.Header> */}
 
-            <Card.Body>
-              <Row style={{ height: "100%" }}>
-                <Col md="4" lg="4" xl="4" sm="4">
-                  <div className="flex">
-                    <Row className="mb-3">
+              {/* </Card.Header> */}
+
+              <Card.Body>
+                <Row style={{ height: "100%" }}>
+                  <Col md="4" lg="4" xl="4" sm="4">
+                    <div className="flex">
+                      <Row className="mb-3">
+                        <Stack direction="horizontal" gap={3}>
+                          <Form.Control
+                            className="me-auto"
+                            type="text"
+                            placeholder="For new chat Type the Title and click on Add >>> "
+                            value={userTopicInput}
+                            onKeyDown={(e) => {
+                              if (e.key === "Enter") {
+                                e.preventDefault(); // Prevent the default behavior of Enter (e.g., form submission)
+                                if (userTopicInput.trim() !== "") {
+                                  handleUserInputTopicSubmit(e);
+                                }
+                              }
+                            }}
+                            onChange={(e) => setUserTopicInput(e.target.value)}
+                            required
+                          />
+                          <Button
+                            variant="primary"
+                            onClick={(e) => {
+                              e.preventDefault(); // Prevent the default behavior of the button
+                              if (userTopicInput.trim() !== "") {
+                                handleUserInputTopicSubmit(e); // Pass the event object to the handler
+                              }
+                            }}
+                            style={{ height: "40px", maxWidth: "60px" }}
+                          >
+                            Add
+                          </Button>
+                        </Stack>
+                      </Row>
+                      <ListGroup variant="flush">
+                        {topics && topics.length >= 0 ? (
+                          topics.map((topic) => (
+                            <ListGroup.Item
+                              key={topic.conversation_title}
+                              action
+                              active={topic.conversation_title === currentTopic}
+                              onClick={() =>
+                                handleTopicChange(topic.conversation_title)
+                              }
+                            >
+                              {topic.conversation_title}
+                            </ListGroup.Item>
+                          ))
+                        ) : (
+                          <div> loading your chat titles </div>
+                        )}
+                      </ListGroup>
+                    </div>
+                    <div style={{ height: "10px" }}></div>
+                  </Col>
+                  <Col
+                    md="8"
+                    lg="8"
+                    xl="8"
+                    sm="8"
+                    style={{ position: "relative" }}
+                  >
+                    <div
+                      className="chat-messages scrollbar"
+                      style={{ overflowY: "scroll", maxHeight: "75vh" }}
+                    >
+                      {convertMessage[currentTopic] ? (
+                        convertMessage[currentTopic].map((message, index) => (
+                          <ChatMessage
+                            key={index}
+                            message={message.message}
+                            sender={message.sender}
+                          />
+                        ))
+                      ) : (
+                        <>
+                          <div
+                            style={{ height: "20px", marginBottom: "5px" }}
+                          ></div>
+                          <div className="d-flex justify-content-center align-items-center">
+                            <div>Start new Conversation</div>
+                          </div>
+                        </>
+                      )}
+                      <div ref={messagesEndRef}></div>
+                    </div>
+                    <div style={{ height: "40px" }}></div>
+                    <div
+                      className="input-section"
+                      style={{
+                        position: "absolute",
+                        bottom: 0,
+                        right: 0,
+                        width: "100%",
+                        marginTop: "10px",
+                      }}
+                    >
                       <Stack direction="horizontal" gap={3}>
                         <Form.Control
                           className="me-auto"
                           type="text"
-                          placeholder="For new chat : Type the Title and click on Add >>> "
-                          value={userTopicInput}
+                          placeholder="Type your message"
+                          value={userInput}
+                          // onKeyDown={handleKeyDown} // Attach the event listener here
                           onKeyDown={(e) => {
                             if (e.key === "Enter") {
                               e.preventDefault(); // Prevent the default behavior of Enter (e.g., form submission)
-                              if (userTopicInput.trim() !== "") {
-                                handleUserInputTopicSubmit(e);
+                              if (userInput.trim() !== "") {
+                                // Check for a non-empty input
+                                handleUserInputSubmit(e);
                               }
                             }
                           }}
-                          onChange={(e) => setUserTopicInput(e.target.value)}
-                          required
+                          onChange={(e) => setUserInput(e.target.value)}
                         />
                         <Button
                           variant="primary"
                           onClick={(e) => {
-                            e.preventDefault(); // Prevent the default behavior of the button
-                            if (userTopicInput.trim() !== "") {
-                              handleUserInputTopicSubmit(e); // Pass the event object to the handler
-                            }
-                          }}
-                          style={{ height: "40px", maxWidth: "60px" }}
-                        >
-                          Add
-                        </Button>
-                      </Stack>
-                    </Row>
-                    <ListGroup variant="flush">
-                      {topics && topics.length >= 0 ? (
-                        topics.map((topic) => (
-                          <ListGroup.Item
-                            key={topic.conversation_title}
-                            action
-                            active={topic.conversation_title === currentTopic}
-                            onClick={() =>
-                              handleTopicChange(topic.conversation_title)
-                            }
-                          >
-                            {topic.conversation_title}
-                          </ListGroup.Item>
-                        ))
-                      ) : (
-                        <div> loading your chat titles </div>
-                      )}
-                    </ListGroup>
-                  </div>
-                  <div style={{ height: "10px" }}></div>
-                </Col>
-                <Col
-                  md="8"
-                  lg="8"
-                  xl="8"
-                  sm="8"
-                  style={{ position: "relative" }}
-                >
-                  <div
-                    className="chat-messages scrollbar"
-                    style={{ overflowY: "scroll", maxHeight: "75vh" }}
-                  >
-                    {convertMessage[currentTopic] ? (
-                      convertMessage[currentTopic].map((message, index) => (
-                        <ChatMessage
-                          key={index}
-                          message={message.message}
-                          sender={message.sender}
-                        />
-                      ))
-                    ) : (
-                      <>
-                        <div
-                          style={{ height: "20px", marginBottom: "5px" }}
-                        ></div>
-                        <div className="d-flex justify-content-center align-items-center">
-                          <div>Start new Conversation</div>
-                        </div>
-                      </>
-                    )}
-                    <div ref={messagesEndRef}></div>
-                  </div>
-                  <div style={{ height: "40px" }}></div>
-                  <div
-                    className="input-section"
-                    style={{
-                      position: "absolute",
-                      bottom: 0,
-                      right: 0,
-                      width: "100%",
-                      marginTop: "10px",
-                    }}
-                  >
-                    <Stack direction="horizontal" gap={3}>
-                      <Form.Control
-                        className="me-auto"
-                        type="text"
-                        placeholder="Type your message"
-                        value={userInput}
-                        // onKeyDown={handleKeyDown} // Attach the event listener here
-                        onKeyDown={(e) => {
-                          if (e.key === "Enter") {
-                            e.preventDefault(); // Prevent the default behavior of Enter (e.g., form submission)
+                            e.preventDefault();
                             if (userInput.trim() !== "") {
                               // Check for a non-empty input
                               handleUserInputSubmit(e);
                             }
-                          }
-                        }}
-                        onChange={(e) => setUserInput(e.target.value)}
-                      />
-                      <Button
-                        variant="primary"
-                        onClick={(e) => {
-                          e.preventDefault();
-                          if (userInput.trim() !== "") {
-                            // Check for a non-empty input
-                            handleUserInputSubmit(e);
-                          }
-                        }}
-                        style={{ height: "40px", maxWidth: "100px" }}
-                      >
-                        Submit
-                      </Button>
-                      <VoiceInput setUserInput={setUserInput} />
-                    </Stack>
-                  </div>
-                </Col>
-              </Row>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+                          }}
+                          style={{ height: "40px", maxWidth: "100px" }}
+                        >
+                          Submit
+                        </Button>
+                        <VoiceInput setUserInput={setUserInput} />
+                      </Stack>
+                    </div>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
-
   );
 };
 
